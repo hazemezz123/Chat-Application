5c16210d567b5e6deb2fd619af63391caf451f27
hazemezz123 <hazemezz988@gmail.com>
2025-07-01 01:07:49 +0300

    feat: initialize server with user authentication and profile management
    - Added package.json for server dependencies and scripts.
    - Implemented user signup, login, logout, and profile update functionalities in auth.controller.js.
    - Created middleware for protecting routes and checking authentication.
    - Established MongoDB connection and configured environment variables.
    - Integrated Cloudinary for profile picture uploads.
    - Defined user model with Mongoose.
    - Set up Express routes for authentication.
    - Added utility functions for JWT generation and error handling.

---
+++ server/src/lib/utils.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 60 * 60 * 1000, // MS
+    httpOnly: true,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development",
+  });
+  return token;
+};
+export const handleServerError = (res, error, context = "") => {
+  console.error(`${context} Error:`, error.message);
+  return res.status(500).json({ message: "Internal Server Error" });
+};
