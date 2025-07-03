e563d2ddbd840164813f6f548a6d54af70440eeb
hazemezz123 <hazemezz988@gmail.com>
2025-07-03 17:53:10 +0300

    Update axios base URL and remove production static file serving

--- server/src/index.js
+++ server/src/index.js
@@ -24,12 +24,6 @@ app.use("/api/messages", messageRoutes);
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-  });
-}
 
 connectDB()
   .then(() => {
