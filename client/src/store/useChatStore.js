526897a1723839c086379c9a095a5429ccad9752
hazemezz123 <hazemezz988@gmail.com>
2025-07-04 06:08:22 +0300

    ..Commit For deploy ان شاء الله

--- client/src/store/useChatStore.js
+++ client/src/store/useChatStore.js
@@ -61,14 +61,6 @@ export const useChatStore = create((set, get) => ({
         set({
           messages: [...get().messages, newMessage],
         });
-      } else {
-        // Show a toast if the message is for the logged-in user but not the selected chat
-        const authUser = useAuthStore.getState().user;
-        if (newMessage.receiverId === authUser._id) {
-          toast.success(
-            `New message from ${newMessage.senderName || "a user"}`
-          );
-        }
       }
     });
   },
