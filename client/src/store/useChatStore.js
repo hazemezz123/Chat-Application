fd361c3a745aee77feaffe93aa7b1b1f5c85e6ae
hazemezz123 <hazemezz988@gmail.com>
2025-07-04 06:03:44 +0300

    ..

--- client/src/store/useChatStore.js
+++ client/src/store/useChatStore.js
@@ -52,24 +52,14 @@ export const useChatStore = create((set, get) => ({
     const { selectedUser } = get();
     if (!selectedUser) return;
     const socket = useAuthStore.getState().socket;
+    // optimize
     socket.on("newMessage", (newMessage) => {
-      // Always add the message if it's for the selected chat
-      if (
-        newMessage.senderId === selectedUser._id ||
-        newMessage.receiverId === selectedUser._id
-      ) {
-        set({
-          messages: [...get().messages, newMessage],
-        });
-      } else {
-        // Show a toast if the message is for the logged-in user but not the selected chat
-        const authUser = useAuthStore.getState().user;
-        if (newMessage.receiverId === authUser._id) {
-          toast.success(
-            `New message from ${newMessage.senderName || "a user"}`
-          );
-        }
-      }
+      const isMessageSendFromSelectedUser =
+        newMessage.senderId !== selectedUser._id;
+      if (!isMessageSendFromSelectedUser) return;
+      set({
+        messages: [...get().messages, newMessage],
+      });
     });
   },
   unsubscribeFromMessages: () => {
